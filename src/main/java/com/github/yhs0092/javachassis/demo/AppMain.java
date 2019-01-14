2f5c7d9053457b755eca7ab56fd895be6e5b752b
yaohaishi <yaohaishi@huawei.com>
2019-01-14 11:22:03 +0800

    modify main class: 1. add @EnableServiceComb

--- src/main/java/com/github/yhs0092/javachassis/demo/AppMain.java
+++ src/main/java/com/github/yhs0092/javachassis/demo/AppMain.java
@@ -1,9 +1,11 @@
 package com.github.yhs0092.javachassis.demo;
 
+import org.apache.servicecomb.springboot2.starter.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableServiceComb
 public class AppMain {
   public static void main(String[] args) {
     SpringApplication.run(AppMain.class, args);
