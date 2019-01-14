f5f7237f648e084e15d2d00ff4092d41ccbab469
yaohaishi <yaohaishi@huawei.com>
2019-01-14 11:22:03 +0800

    modify main class: 1. exclude DispatcherServletAutoConfiguration(not essential but recommended) 2. add @EnableServiceComb

--- src/main/java/com/github/yhs0092/javachassis/demo/AppMain.java
+++ src/main/java/com/github/yhs0092/javachassis/demo/AppMain.java
@@ -1,9 +1,12 @@
 package com.github.yhs0092.javachassis.demo;
 
+import org.apache.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DispatcherServletAutoConfiguration.class)
+@EnableServiceComb
 public class AppMain {
   public static void main(String[] args) {
     SpringApplication.run(AppMain.class, args);
